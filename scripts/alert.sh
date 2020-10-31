51d5a878dc330c3aa4126acabe582d70ce94827c
João Santos <joaopns05@gmail.com>
2020-10-31 20:33:02 +0000

    Add alert script

---
+++ scripts/alert.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+blogUrl="https://xpto.joaonsantos.dev"
+dateNow=$(command date)
+
+if res=$(curl -f --url "$blogUrl"); then
+	echo "$dateNow - success" >> /var/log/blog-alerts.log
+else
+	mail -s "[ALERT] Blog is down" -a "From: mail@joaonsantos.dev" joaopns05@gmail.com \
+        <<< "Blog is down! joaonsantos.dev is not reachable."
+fi
+
