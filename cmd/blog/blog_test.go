21f2127c4095c3d39deb33045fc5e11bc6f646cd
João Santos <joaopns05@gmail.com>
2021-04-02 18:45:09 +0100

    test implementation complete

--- cmd/blog/blog_test.go
+++ cmd/blog/blog_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	"net/http/httptest"
@@ -32,7 +33,22 @@ func initTables() {
 }
 
 func clearTables() {
-	a.DB.Exec("DELETE FROM posts")
+	a.DB.Exec("delete from posts")
+}
+
+func addPosts(count int) {
+	for i := 0; i < count; i++ {
+		a.DB.Exec(`insert into posts(id, title, body, summary, author, readTime, dateModified)
+		values($1,$2,$3,$4,$5,$6,$7)`,
+			fmt.Sprintf("test-%v", i),
+			fmt.Sprintf("Test %v", i),
+			fmt.Sprintf("Test Content %v", i),
+			fmt.Sprintf("Summary %v", i),
+			"Test Author",
+			1,
+			1617382428,
+		)
+	}
 }
 
 func doRequest(r *http.Request) *httptest.ResponseRecorder {
@@ -76,6 +92,39 @@ func TestGetNonExistentPost(t *testing.T) {
 	json.Unmarshal(rr.Body.Bytes(), &m)
 	if len(m) != 0 {
 		t.Errorf("Expected the response to be an empty json. Got '%v'.", m)
+	}
+}
+
+func TestGetPosts(t *testing.T) {
+	clearTables()
+	addPosts(2)
+
+	req := httptest.NewRequest(
+		http.MethodGet,
+		"/api/v1/posts/",
+		nil,
+	)
+
+	rr := doRequest(req)
+	checkResponseCode(t, http.StatusOK, rr.Code)
+
+	var l []map[string]interface{}
+	json.Unmarshal(rr.Body.Bytes(), &l)
+
+	if s := len(l); s != 2 {
+		t.Errorf("Expected to get two posts. Got '%v'.", s)
+	}
+
+	for i := range l {
+		m := l[i]
+
+		if m["id"] != fmt.Sprintf("test-%v", i) {
+			t.Errorf("Expected the post id to be 'test-1'. Got '%v'.", m["id"])
+		}
+
+		if m["readTime"] != 1 {
+			t.Errorf("Expected the post read time to be '1'. Got '%v'.", m["readTime"])
+		}
 	}
 }
 
@@ -131,11 +180,38 @@ func TestCreatePost(t *testing.T) {
 	}
 
 	if m["readTime"] != 1 {
-		t.Errorf("Expected the post read time to be '0'. Got '%v'.", m["readTime"])
+		t.Errorf("Expected the post read time to be '1'. Got '%v'.", m["readTime"])
+	}
+}
+
+func TestGetPost(t *testing.T) {
+	clearTables()
+	addPosts(1)
+
+	req := httptest.NewRequest(
+		http.MethodGet,
+		"/api/v1/post/test-1",
+		nil,
+	)
+
+	rr := doRequest(req)
+	checkResponseCode(t, http.StatusOK, rr.Code)
+
+	var m map[string]interface{}
+	json.Unmarshal(rr.Body.Bytes(), &m)
+	if m["id"] != "test-1" {
+		t.Errorf("Expected the post id to be 'test-1'. Got '%v'.", m["id"])
+	}
+
+	if m["readTime"] != 1 {
+		t.Errorf("Expected the post read time to be '1'. Got '%v'.", m["readTime"])
 	}
 }
 
 func TestUpdatePost(t *testing.T) {
+	clearTables()
+	addPosts(1)
+
 	postJSON := []byte(`{
 		"summary": "What makes up a test.",
 		"body": "This is the content of the test post."
@@ -143,7 +219,7 @@ func TestUpdatePost(t *testing.T) {
 
 	req := httptest.NewRequest(
 		http.MethodPatch,
-		"/api/v1/post/programming-is-more-than-syntax",
+		"/api/v1/post/test-1",
 		bytes.NewBuffer(postJSON),
 	)
 	req.Header.Set("Content-Type", "application/json")
@@ -153,8 +229,8 @@ func TestUpdatePost(t *testing.T) {
 
 	var m map[string]interface{}
 	json.Unmarshal(rr.Body.Bytes(), &m)
-	if m["id"] != "programming-is-more-than-syntax" {
-		t.Errorf("Expected the post id to be 'programming-is-more-than-syntax'. Got '%v'.", m["id"])
+	if m["id"] != "test-1" {
+		t.Errorf("Expected the post id to be 'test-1'. Got '%v'.", m["id"])
 	}
 
 	if m["summary"] != "What makes up a test." {
